35f85daa49eb3cb63f8b9452b046fbbd01842df5
mthom26 <mthom2626@gmail.com>
2021-04-15 18:57:21 +0100

    Add random pos/target to enemy missiles

--- src/enemy_spawner.rs
+++ src/enemy_spawner.rs
@@ -1,4 +1,5 @@
 use bevy::{prelude::*, utils::Duration};
+use rand::prelude::*;
 
 use crate::{missile::SpawnMissile, team::Team};
 
@@ -18,13 +19,23 @@ impl Plugin for EnemySpawnerPlugin {
 
 fn update_timer(
     time: Res<Time>,
+    windows: Res<Windows>,
     mut spawner: ResMut<EnemyMissileSpawner>,
     mut events: EventWriter<SpawnMissile>,
 ) {
     if spawner.timer.tick(time.delta()).finished() {
+        let (half_width, half_height) = if let Some(window) = windows.get_primary() {
+            (window.width() / 2.0, window.height() / 2.0)
+        } else {
+            panic!("Could not get primary window!");
+        };
+        let mut rng = thread_rng();
+        let x_pos = rng.gen_range(-half_width..half_width);
+        let x_tar = rng.gen_range(-half_width..half_width);
+
         events.send(SpawnMissile {
-            position: Vec3::new(0.0, 300.0, 0.0),
-            target: Vec3::new(0.0, -300.0, 0.0),
+            position: Vec3::new(x_pos, half_height, 0.0),
+            target: Vec3::new(x_tar, -half_height, 0.0),
             team: Team::Enemy,
         });
     }
